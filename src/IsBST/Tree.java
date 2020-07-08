f9de94b77e686314095222c3ffd9fdd578306178
user <user@192.168.0.115>
2020-07-08 20:52:28 +0100

    Adding more BST algorithms

---
+++ src/IsBST/Tree.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IsBST;
+
+/**
+ *
+ * @author user
+ */
+// { Driver Code Starts
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+        	    if(g.isBST(root))
+			        System.out.println(1);
+			    else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Structure of Node:
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+public class Tree
+{
+    // return true if the given tree is a BST, else return false
+    boolean isBST(Node root)
+        {
+        if(root == null) return true;
+        return isBstValid(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+ 
+    private static boolean isBstValid(Node root, Integer minValue, Integer maxValue) 
+    {
+        if(root == null) return true;
+        if(root.data >= minValue && root.data < maxValue
+                && isBstValid(root.left, minValue, root.data)
+                && isBstValid(root.right, root.data, maxValue)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+//Example:
+//Input:
+//2
+//2 1 3
+//2 N 7 N 6 N 5 N 9 N 2 N 6
+//Output:
+//1
+//0
---
+++ src/SameCharacters/SameCharacter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SameCharacters;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public class SameCharacter {
+
+    public static void main(String[] args) {
+        String s1="mihaela";
+        String s2="ealamih";
+        System.out.println(sameChars(s1,s2));
+    }
+
+    private static boolean sameChars(String firstStr, String secondStr) {
+        char[] first = firstStr.toCharArray();
+        char[] second = secondStr.toCharArray();
+        Arrays.sort(first);
+        Arrays.sort(second);
+        return Arrays.equals(first, second);
+    }
+}
